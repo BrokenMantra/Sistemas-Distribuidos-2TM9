41409eae8f240875cdc95f3ef5aa631b44559370
BrokenMantra <92129122+BrokenMantra@users.noreply.github.com>
2022-03-07 16:59:25 -0600

    Práctica 2
    Práctica 2
    Ceballos Romero Alvaro.
    2TM9

---
+++ sockets/cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sockets;
+
+/**
+ *
+ * @author Rimuru
+ */
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class cliente {
+
+    static OutputStream getOutputStream() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    FrameCliente c =new FrameCliente();
+    
+}
